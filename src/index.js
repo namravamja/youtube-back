a9cc690bacc3918ae84f49c7d328cc79d0e90659
NamraVamja <namravamja00@gmail.com>
2025-02-12 00:26:09 +0530

    refactor: restructure application by removing unused files and adding environment configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
