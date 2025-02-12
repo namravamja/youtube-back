de46f82e835ea09b74691eae12aab100a243dd11
NamraVamja <namravamja00@gmail.com>
2025-02-12 18:28:05 +0530

    feat: add MongoDB connection and update environment configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import { connect } from "mongoose";
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
