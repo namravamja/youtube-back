14b8bf1feea6018d679197b242454ab84942dcf2
NamraVamja <namravamja00@gmail.com>
2025-02-18 09:55:36 +0530

    feat: add various new modules and files to dependencies, including bcrypt and jsonwebtoken

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -46,5 +47,17 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next;
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+
 
 export const User = mongoose.model("User", userSchema);
