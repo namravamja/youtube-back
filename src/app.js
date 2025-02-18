c07b531399f4e194f947d803502e39c4a76c5270
NamraVamja <namravamja00@gmail.com>
2025-02-18 11:00:40 +0530

    feat: add multer for file uploads and integrate cookie-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 
@@ -14,6 +16,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser)
 
 // Helth check
 // routes
